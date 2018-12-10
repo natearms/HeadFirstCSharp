681a5eb74cb16879d7a8cc3897ad2a0d89664305
Nathaniel Arms <nathaniel.arms@gmail.com>
2018-12-09 21:17:53 -0600

    Save The Humans

--- Save The Humans/Save The Humans/Properties/AssemblyInfo.cs
+++ Save The Humans/Save The Humans/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Save The Humans")]
+[assembly: AssemblyTitle("Save the Humans")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Save The Humans")]
+[assembly: AssemblyProduct("Save the Humans")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
